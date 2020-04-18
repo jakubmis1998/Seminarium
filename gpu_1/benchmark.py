4577a80fc66e62b04fd92ae6786438057b66338b
Jakub <jakubmis98@wp.pl>
2020-04-18 19:04:49 +0200

    shared matmul update with cpu matmul without numpy

--- gpu_1/benchmark.py
+++ gpu_1/benchmark.py
@@ -7,7 +7,7 @@ import numpy as np, time
 import skcuda.linalg as linalg
 linalg.init()
 
-dim = 100
+dim = 16384
 a = np.random.rand(dim, dim).astype(np.float32) # array
 
 # GPU wkopiowanie danych
@@ -43,30 +43,30 @@ błędy rzędu e^-13 / e^-11
 bardzo wolne wykopiowanie
 
 N = 100
-CPU : 0.00122 s.
-Copy in : 0.00066 s.
-GPU : 0.32092 s.
-Copy out : 0.00012 s.
+CPU :       0.00122 s.
+Copy in :   0.00066 s.
+GPU :       0.32092 s.
+Copy out :  0.00012 s.
 True
 
 N = 10 000
-CPU : 2.091 s.
-Copy in : 0.063 s.
-GPU : 0.427 s.
-Copy out : 0.140 s.
+CPU :       2.091 s.
+Copy in :   0.063 s.
+GPU :       0.427 s.
+Copy out :  0.140 s.
 True
 
 N = 20 000
-CPU : 15.121 s.
-Copy in : 0.261 s.
-GPU : 2.453 s.
-Copy out : 0.583 s.
+CPU :       15.121 s.
+Copy in :   0.261 s.
+GPU :       2.453 s.
+Copy out :  0.583 s.
 False
 
 N = 30 000
-CPU : 70.330 s.
-Copy in : 0.584 s.
-GPU : 8.379 s.
-Copy out : 1.330 s.
+CPU :       70.330 s.
+Copy in :   0.584 s.
+GPU :       8.379 s.
+Copy out :  1.330 s.
 False
 """
