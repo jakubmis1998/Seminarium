69dc2f33244a6f0cc3e6d8a4befc9c9a854cb5da
Jakub <jakubmis98@wp.pl>
2020-04-28 18:49:21 +0200

    opencl fun

--- opencl/matMulPyviennacl.py
+++ opencl/matMulPyviennacl.py
@@ -1,15 +1,10 @@
-# Use OpenCL To Multiplicate Two Random Matrixes
+# Use PyViennaCL To Multiplicate Two Random Matrixes
 
-import numpy as np  # Import Numpy tools
+import numpy as np
 import pyviennacl as vienna
 import pyviennacl.linalg as linalg  # Import PyViennaCl (Python ViennaCl library for Linear Algebra)
 import time
-import pyopencl as cl  # Import the OpenCL GPU computing API
-import pyopencl.array as pycl_array
-platform = cl.get_platforms()[0]  # Select the first platform [0]
-device = platform.get_devices()[0]  # Select the first device on this platform [0]
-context = cl.Context([device])  # Create a context with your device
-queue = cl.CommandQueue(context)  # Instantiate a Queue
+
 # Initialize array size
 N = 2
 
@@ -23,13 +18,15 @@ print("CPU: %.7f s" % (time.time() - s))
 
 gpu_time = time.time()
 
+a = [[1,2],[3,4]]
+
 # Create two random pyviennacl matrixes
-a_gpu = vienna.Matrix(a_cpu)
-b_gpu = vienna.Matrix(b_cpu)
+a_gpu = vienna.Matrix(a)
+b_gpu = vienna.Matrix(a)
 print("copy in: %.7f s" % (time.time() - gpu_time))
 # Create an empty pyopencl destination array 
 result_gpu = linalg.prod(a_gpu, b_gpu).as_ndarray()
 # result_gpu = (a_gpu * b_gpu).as_ndarray()
-print("GPU: %.7f s" % (time.time() - gpu_time))
+print("GPU compute: %.7f s" % (time.time() - gpu_time))
 
 print("Computation error:\n {}".format(abs(np.subtract(result_cpu, result_gpu))))
